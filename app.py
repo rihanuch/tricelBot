964b16efe5c16bb15b1b1ed96b626176cb495063
rihanuch <rihanuch@uc.cl>
2020-10-08 14:51:48 -0300

    initial commit 🎉

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import telegram
+from telebot.simple import get_response
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+global bot
+global TOKEN
+
+
+TOKEN = os.getenv("bot_token")
+URL = os.getenv("URL")
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    response = get_response(text)
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return f'.'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
