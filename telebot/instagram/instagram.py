101045036e5a2399baf48ee54743093144de0130
rihanuch <rihanuch@uc.cl>
2020-10-18 02:25:28 -0300

    [instagram] add: proxies + modularization

--- telebot/instagram/instagram.py
+++ telebot/instagram/instagram.py
@@ -1,19 +1,21 @@
 import os
 import json
 from datetime import datetime
+from telebot.settings import IG_USER, IG_PWD, IG_FILE_LOC, PROXIES
 
-IG_FILE_LOC = os.getcwd() + '/downloads/instagram/'
 
+def judge(update, context, *args):
+    # unpack and assign default values
+    accounts, *amount = args
+    amount = amount[0] if amount else 1
 
-def judge(update, context, user, pwd, accounts):
-    accounts = '' if accounts == [] else ','.join(
-        [acc.strip().replace(',', '') for acc in accounts])
-    os.system(f'instagram-scraper {accounts} -d {IG_FILE_LOC} -n --latest --media-types none --comments --comments -m 5')
+    command = f'instagram-scraper {accounts} -d {IG_FILE_LOC} -n --latest --media-types none --comments --comments -m {amount} --proxies \'{PROXIES}\''
+    os.system(command)
     for watched in os.listdir(IG_FILE_LOC):
         if '.keep' in watched:
             continue
-        pictures_with_comments(
-            update, context, f'{IG_FILE_LOC}{watched}/{watched}.json')
+        pictures_with_comments(update, context, f'{IG_FILE_LOC}{watched}/{watched}.json')
+
 
 def pictures_with_comments(update, context, file):
     with open(file) as json_file:
@@ -26,4 +28,4 @@ def pictures_with_comments(update, context, file):
             comments = [f'<b>{cmt["owner"]["username"]}</b>: {cmt["text"]}' for cmt in image['comments']['data']]
             comments = [f'{i+1}) {cmt}' for i, cmt in enumerate(comments)]
             # TODO: cut if message is over 20 comments long
-            update.message.reply_text(f'{header}\n'+ '\n'.join(comments), parse_mode='HTML')
+            update.message.reply_text(f'{header}\n' + '\n'.join(comments), parse_mode='HTML')
