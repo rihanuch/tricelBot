ba888be1d8853ec8b439cd20d6bb7610aaf4be4f
rihanuch <rihanuch@uc.cl>
2020-10-18 02:24:08 -0300

    [settings] add: modularization

---
+++ telebot/settings.py
@@ -0,0 +1,22 @@
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PORT = int(os.getenv('PORT', 5000))
+TOKEN = os.getenv('BOT_TOKEN')
+URL = os.getenv('URL')
+
+############################################################
+############## SOCIAL MEDIA SPECIFIC ACCOUNTS ##############
+
+PROXIES = json.dumps({'http': os.getenv('QUOTAGUARD_URL')})
+
+######################## INSTAGRAM ########################
+
+IG_USER = os.getenv('IG_USER')
+IG_PWD = os.getenv('IG_PWD')
+IG_FILE_LOC = os.getcwd() + '/downloads/instagram/'
+
+############################################################
